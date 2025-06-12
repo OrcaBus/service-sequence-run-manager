3ceb0aafc179a28815c36821ac6b6be6ee06de1e
Ray Liu <ray.liu@umccr.org>
2025-06-13 03:10:32 +1000

    fix check error

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
-export const testEnvironment = 'node';
-export const roots = ['<rootDir>/test'];
-export const testMatch = ['**/*.test.ts'];
-export const transform = {
-  '^.+\\.tsx?$': 'ts-jest',
+/* eslint-disable-next-line no-undef */
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
